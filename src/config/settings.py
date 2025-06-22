f50ae3f06bc90a5f8347a536f792459794dba8eb
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2025-06-22 16:24:14 +0200

    refactor(config): Change Settings to use Database

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,24 +1,10 @@
-import os
-import json
-
+from data.db import Database
 class Settings:
     def __init__(self):
-        self.config_path = os.path.join(os.path.dirname(__file__), 'settings.json')
-        self.data = self._load()
-    
-    def _load(self):
-        if os.path.exists(self.config_path):
-            with open(self.config_path, 'r', encoding='utf-8') as f:
-                return json.load(f)
-        return {}
-    
-    def _save(self):
-        with open(self.config_path, 'w', encoding='utf-8') as f:
-            json.dump(self.data, f, indent=4)
-    
+        self.db = Database()
+
     def get_language(self):
-        return self.data.get("language", "en")
+        return self.db.get_setting("language", "en")
     
     def set_language(self, language):
-        self.data["language"] = language
-        self._save()
+        self.db.set_setting("language", language)
