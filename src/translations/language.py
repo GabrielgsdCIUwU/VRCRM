99f9182c084b7c3c97712f767e422d26cffd56f9
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2025-06-22 13:49:37 +0200

    refactor(translations): Change Language to use settings

--- src/translations/language.py
+++ src/translations/language.py
@@ -1,16 +1,19 @@
 import json
 import os
+from config.settings import Settings
 
 class Language:
     _instance = None
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(Language, cls).__new__(cls)
-            cls._instance.language = "en"
+            cls._instance.settings = Settings()
+            cls._instance.language = cls._instance.settings.get_language()
         return cls._instance
     
     def set_language(self, language):
         self.language = language
+        self.settings.set_language(language)
     
     def get_language(self):
         return self.language
@@ -20,5 +23,6 @@ class Language:
             with open(os.path.join(os.path.dirname(__file__), 'languages' , f"{self.get_language()}.json"), 'r', encoding='utf-8') as f:
                 return json.load(f)
         except Exception as e:
-            return print(f"Error loading translations: {e}")
+            print(f"Error loading translations: {e}")
+            return {}
 
